d6931f3c6ede153f93dec27c48925c4c5aa113a3
iljeboi <50242003+iljeboi@users.noreply.github.com>
2021-09-28 00:46:43 -0400

    website update

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,25 @@
 import PageWrapper from './components/PageWrapper';
-import Home from './components/pages/Home';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
+
 function App() {
   return (
     <Router>
       <PageWrapper>
         <Route 
+          exact={true}
           path="/"
           component={Home}
         />
+
+        <Route
+          path="/about"
+          component={About}
+        />
+
       </PageWrapper>
     </Router>
   );
--- website/src/components/pages/Home.js
+++ website/src/components/pages/Home.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-
+import Header from "../common/Header";
+import img from "../assets/img/header-bg.jpg";
 class Home extends Component{
     render(){
         return(
             <div>
-                <header className="masthead">
-                    <div className="container">
-                    <div className="masthead-subheading">Welcome To Our Studio!</div>
-                    <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                        <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
-                    </div>
-                </header>
+                <Header 
+                    title= "Welcome to Our Studio"
+                    subtitle= "IT'S NICE TO MEET YOU"
+                    buttonText="TELL ME MORE"
+                    link="/services"
+                    showButton={true}
+                    image={img}
+                />
             </div>
         )
     }
